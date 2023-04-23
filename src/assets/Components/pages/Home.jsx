f67e4baf0ea694de337c3ec2aafe339a7ee8ad58
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-04-23 07:39:49 +0500

    first commit

--- src/assets/Components/pages/Home.jsx
+++ src/assets/Components/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import './Home.css'
+import Video from '../../videos/hero_bg_video.mp4'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>This is Home page!</p>
-        </div>
+        <>
+            <video src={Video} autoPlay loop muted></video>
+            <div className='home-section'>
+                <h1>
+                    Nature <i class="fa-sharp fa-solid fa-leaf"></i>
+                </h1>
+                <h3>This website is about nature</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, eveniet?</p>
+                <a href="">Get Started</a>
+                <a href="">Watch Video <i class="fa-solid fa-circle-play"></i></a>
+            </div>
+        </>
     );
 };
 
