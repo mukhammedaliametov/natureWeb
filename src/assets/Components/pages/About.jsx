deef3bf9edeed305bcc0297ac52016298f65b2fb
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-04-23 11:27:11 +0500

    first commit

--- src/assets/Components/pages/About.jsx
+++ src/assets/Components/pages/About.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './About.css'
+import Image from '../../images/img-1.jpg'
+import Image2 from '../../images/img-2.jpg'
 
 const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <p>This is About page!</p>
+
+  const [click, setClick] = useState(false)
+
+  const imgClick = () => setClick(!click)
+
+  return (
+    <>
+      <div className='section-head'>
+        <h1>About</h1>
+      </div>
+      <div className="about-section">
+        <div className="first-about">
+          <div className="images">
+            <img src={Image} alt="" className='img-1'/>
+            <img src={Image2} alt="" className='img-2'/>
+          </div>
+          <div className="title">
+            <h1>Nature <i class="fa-sharp fa-solid fa-leaf"></i></h1><br />
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore adipisci tempore reprehenderit magnam quia odit officiis animi accusantium aliquid ducimus.</p><br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates obcaecati nisi aut fugit veniam debitis magni, odit nesciunt, tempore sint at exercitationem. Deleniti repudiandae maxime dolor odit similique molestias quam!</p>
+          </div>
         </div>
-    );
+        <div className="second-about">
+          <div className="title">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem sequi laudantium laborum quasi iusto unde dignissimos autem quo minima quae?</p><br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam praesentium magni atque ipsum dolorem ab accusantium laboriosam at quisquam libero. Accusamus voluptas vero sit quod totam dolor quaerat ducimus repellat!</p>
+          </div>
+          <div className="imgages">
+            <img src={Image} alt="" className='img-3'/>
+            <img src={Image2} alt="" className='img-4'/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
